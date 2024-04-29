6ac304fdd7e39dd73ad47f52d7c83121e903c0ab
Vikas Saini <vikas08218@gmail.com>
2024-04-29 15:00:30 +0530

    Submit

---
+++ src/com/coding/Compare_String_17.java
@@ -0,0 +1,19 @@
+package com.coding;
+
+public class Compare_String_17 {
+
+	public static void main(String[] args) {
+		String str ="vikas";
+		String str2="vikas";
+				if(str==str2) {
+					System.out.println("It is Compare String");
+				}
+					else
+						System.out.println("It is not Compare String");
+				
+			}
+		
+		
+	
+
+}
--- src/com/coding/Swap_String_pari_to_characters_11.java
+++ src/com/coding/Swap_String_pari_to_characters_11.java
@@ -2,21 +2,14 @@ package com.coding;
 public class Swap_String_pari_to_characters_11 {
 	    public static void main(String[] args) {
 	        String str = "Hello, world!";
-	        
-	        if (str == null || str.length() <= 1) {
-	            System.out.println("Original string: " + str);
-	            System.out.println("String after swapping pairs: " + str);
-	            return;
-	        }
-
-	        char[] charArray = str.toCharArray();
-	        for (int i = 0; i < charArray.length - 1; i += 2) {
-	            char temp = charArray[i];
-	            charArray[i] = charArray[i + 1];
-	            charArray[i + 1] = temp;
+	        char[] ch= str.toCharArray();
+	        for (int i = 0; i < ch.length - 1; i =i+ 2) {
+	            char temp = ch[i];
+	            ch[i] = ch[i + 1];
+	            ch[i + 1] = temp;
 	        }
 	        
 	        System.out.print("Original string: " + str + "\nString after swapping pairs: ");
-	        System.out.println(charArray);
+	        System.out.println(ch);
 	    }
 	}
