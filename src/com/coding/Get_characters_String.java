e4e6dc126128b9276c67d8fc63ee4e1a941bdf3d
Vikas Saini <vikas08218@gmail.com>
2024-04-30 14:58:54 +0530

    Get characters

---
+++ src/com/coding/Get_characters_String.java
@@ -0,0 +1,11 @@
+package com.coding;
+
+public class Get_characters_String {
+
+	public static void main(String[] args) {
+			String str = "Hello!";
+			char ch = str. charAt(4);
+			System. out. println(ch);
+			}
+			
+	}
