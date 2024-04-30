597015a3053c86aa1341bbebca144de941896898
Vikas Saini <vikas08218@gmail.com>
2024-04-30 09:55:58 +0530

    Submit

---
+++ src/com/coding/Unicode_code_string_index_find.java
@@ -0,0 +1,13 @@
+package com.coding;
+
+public class Unicode_code_string_index_find {
+
+	public static void main(String[] args) {
+		String str = "vikassaini";
+			System.out.println((int)str.charAt(5));
+			
+		}
+
+	}
+
+
